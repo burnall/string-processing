34c8e074af2303b23c303e91b88ff77457f0a646
Maxim Fainberg <maksym.fainberg@mobenga.com>
2018-09-25 08:47:30 +0200

    Burrows-wheeler - invert better - beginning

--- burrows-wheeler/src/bw/core.clj
+++ burrows-wheeler/src/bw/core.clj
@@ -30,7 +30,25 @@
         (->> (iterate step))
         (nth (count s))
         (first))))
-    
+
+; [2 3 1 1 2] -> :indexByElem{1 [2 3], 2 [0 4], 3 [1]},  :indexByPos [0 0 0 1 1] 
+(defn get-indexes [s]
+  (reduce (fn [{:keys [indexByElem  indexByPos]} ch] 
+             (let [pos (count indexByPos)
+                   elems (get indexByElem ch [])]
+               {:indexByPos (conj indexByPos (count elems)) 
+                :indexByElem (assoc indexByElem ch (conj elems pos))}))
+          {:indexByPos [], :indexByElem {}} 
+          s))
+
+(defn invert-better [s] 
+  (let [ss (sort s)
+        s-i (get-indexes s)
+        ss-i (get-indexes ss)
+       ]
+       22
+))
+
 
 (defn -main
   "I don't do a whole lot."
