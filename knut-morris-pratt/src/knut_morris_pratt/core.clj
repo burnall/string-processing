e62afca69c3db1ac46bfa257f5f143d00a6c69ec
Maxim Fainberg <maksym.fainberg@mobenga.com>
2018-09-30 14:50:35 +0200

    Knut-Morris-Pratt - exact pattern match done

--- knut-morris-pratt/src/knut_morris_pratt/core.clj
+++ knut-morris-pratt/src/knut_morris_pratt/core.clj
@@ -1,6 +1,27 @@
 (ns knut-morris-pratt.core
   (:gen-class))
 
+(defn compute-borders [s]
+  (reduce (fn [borders i]
+            (let [get-border (fn [border]
+                    (cond (= (s border) (s i)) (inc border)
+                          (zero? border) 0
+                         :else (recur (borders (dec border)))))
+                  border (get-border (peek borders))]
+              (conj borders border)))
+          [0]    
+          (range 1 (count s)))) 
+
+(defn kmp [s p] 
+  (let [prefixes (compute-borders (vec (str p "$" s)))
+        len-p (count p)]
+    (->> prefixes 
+         (drop len-p)
+         (map vector (range))
+         (filter (fn [[i prefix]] (= prefix len-p)))
+         (map (fn [[i & _]] (- i len-p))))))
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
