5fc5b429429c17966728ea87b083a9bf4e67d9c7
Maxim Fainberg <maksym.fainberg@mobenga.com>
2018-10-24 11:12:46 +0200

    Suffix array - get-classes

--- suffix-array-and-tree/src/suffix_array_and_tree/core.clj
+++ suffix-array-and-tree/src/suffix_array_and_tree/core.clj
@@ -1,4 +1,4 @@
-(ns suffix-array-and-tree.core
+(ns suffix-array-and-tree.core)
 
 (defn sort-by-char[strings] 
   (let [index-by-char (reduce (fn [m [[c] i]] (update m c (fn [key] (conj (if key key []) i))))  
@@ -7,3 +7,22 @@
     (->> index-by-char
         (vals)
         (apply concat))))
+
+(defn get-classes[strings index-by-char]
+  (->> index-by-char 
+       (reduce (fn [{:keys [last-char last-order classes]} index]
+                 (let [ch (first (strings index))
+                       order (+ last-order (if (= ch last-char) 0 1))]   
+                   {:last-char ch
+                    :last-order order
+                    :classes (conj classes order)}))
+               {:last-char (-> index-by-char (first) (strings) (first))
+                :last-order 0
+                :classes []})
+       (:classes)))         
+
+(defn t [strings]
+  (->> strings
+       (sort-by-char)
+       (get-classes strings)))
+
